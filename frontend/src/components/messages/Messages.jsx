659a9d17e11a37300f74121947873ac2aa1fd7f1
Laiba <laibatariq110@gmail.com>
2026-05-07 19:57:01 +0500

    2nd part completed - ui design

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,26 @@
+import Message from "./Message"
+
+const Messages = () => {
+  return (
+    <div className="messages px-4 flex-1 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  )
+}
+
+export default Messages
